6960d875f24e7937e2e6208a9d4a97c4b1b0b09c
Gaya Kessler <gaya.kessler@gmail.com>
2019-06-25 12:31:56 +0200

    remove ref

--- src/hooks/usePromise.js
+++ src/hooks/usePromise.js
@@ -1,37 +1,29 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 function usePromise(resolver) {
   const [isPending, setPending] = useState(false);
   const [error, setError] = useState(undefined);
   const [resolvedValue, setValue] = useState(undefined);
-  const resolving = useRef(null);
 
   useEffect(
     () => {
-      // if resolver changed and doesn't match the current resolving Promise, trigger effect
-      if (resolving.current !== resolver) {
-        resolving.current = resolver;
+      // reset fetch state values
+      setPending(true);
+      setError(undefined);
 
-        // reset fetch state values
-        setPending(true);
-        setError(undefined);
-
-        resolver()
-          .then((value) => {
-            setValue(value);
-          })
-          .catch(e => {
-            setError(e);
-          })
-          .then(() => {
-            setPending(false);
-
-            // reset current resolving back to false
-            resolving.current = null;
-          });
-      }
+      // resolve the Promise
+      resolver()
+        .then((value) => {
+          setValue(value);
+        })
+        .catch(e => {
+          setError(e);
+        })
+        .then(() => {
+          setPending(false);
+        });
     },
-    // if resolver changes, fetch again
+    // if resolver changes, resolve again
     [resolver],
   );
 
